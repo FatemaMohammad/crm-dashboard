f1f4ac2b0721525527d5fcbe872c5fb318f5087a
Fatema <njbirali11@gmail.com>
2026-07-09 22:46:28 +0200

    Create sidebar container

--- wezimplify-crm-dashboard/src/components/layout/Sidebar.tsx
+++ wezimplify-crm-dashboard/src/components/layout/Sidebar.tsx
@@ -1,5 +1,9 @@
 function Sidebar() {
-  return <aside>Sidebar</aside>;
+  return (
+    <aside className="w-72 h-screen bg-white border-r border-slate-200 flex flex-col">
+      Sidebar
+    </aside>
+  );
 }
 
 export default Sidebar;
